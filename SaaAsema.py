581ef459c9ab39c09c9c65a118c7fbf399d6713f
JormaWuorio <JormaWuorio@users.noreply.github.com>
2021-03-26 17:27:19 +0200

    Lisätty tulostus, listaanlisäys ja teettämiset

--- SaaAsema.py
+++ SaaAsema.py
@@ -4,6 +4,16 @@ class SaaAsema:
         self.tyyppi = tyyppi
         self.sijainti = sijainti 
         self.numero = numero
+    
+    @staticmethod
+    def tee_saaasema():
+        nimi = input("Anna sääaseman nimi: ")
+        tyyppi = input("Anna tyyppi: ")
+        sijainti = input("Anna sijainti: ")
+        numero = input("Anna numero: ")
+        uusi = SaaAsema(nimi, tyyppi, sijainti, numero)
+        return uusi
+        
 
 class SaaHavainto:
     def __init__(self, paivamaara: str, lampotila, tnopeus, tsuunta, pilvisyys, nakyvyys, numero):
@@ -30,33 +40,60 @@ class SaaHavainto:
     def mskmh(self):
         kilometrit = self.tnopeus * 3.6
         return str(kilometrit) + "km/h"
+    
+    @staticmethod
+    def tee_saahavainto():
+        pvm = input("Anna päivämäärä: ")
+        lampo = input("Anna lämpötila: ")
+        tnopeus = input("Anna tuulennopeus: ")
+        tsuunta = input("Anna tuulensuunta: ")
+        pilvisyys = input("Pilvisyys: ")
+        nakyvyys = input("Näkyvyys(kilometrejä): ")
+        numero = int(input("Anna sijainti(1, 2): "))
+        uusi = SaaHavainto(pvm, lampo, tnopeus, tsuunta, pilvisyys, nakyvyys, numero)
 
-pvm=input("Anna päivämäärä: ")
-sijainti=int(input("Anna sijainti: "))
-chica = SaaAsema("chica", "Rannikkoasema", "Chicago", "1")
-nyki = SaaAsema("nyki", "Lentokenttä", "New York", "2")
-"""
-SaaAsemalista =[chica, nyki]
-for asema in SaaAsemalista:
-    print("Sääasema", asema.sijainti)
-"""
+        return uusi
 
-eka = SaaHavainto("23.03.", 20, 5, 320, 3/8, 1, 1)
-toka = SaaHavainto("24.03", 22, 122, 3000, 1/8, 2, 1)
-kolmas = SaaHavainto("25.03.", 21, 90, 240, 4/8, 3, 2)
-neljas = SaaHavainto("26.03.", 23, 97, 180, 6/8, 4, 2)
-viides = SaaHavainto("27.03.", 24, 88, 150, 2/8, 5, 1)
-lista = [eka, toka, kolmas, neljas, viides]
-for havainto in lista:
-    if havainto.paivamaara == pvm and havainto.numero == sijainti:
-      
-        print("Päivämäärä:", havainto.paivamaara)
-        print("Lämpötila:", havainto.lampotila)
-        print("Tuulennopeus:", havainto.tnopeus, "\n")
 
-'''
-print("Säähavainto ilmeni", fasfas.paivamaara + ", lämpötila oli", fasfas.lampotila, "\nTuulennopeus oli tuolloin", str(fasfas.tnopeus) + "m/s ja suunta", fasfas.tsuunta, "astetta")
-print(str(fasfas.tnopeus) + "m/s kilometreinä on", fasfas.mskmh() + ". Pilvisyys oli", fasfas.pilvisyys, "ja näkyvyys", fasfas.nakyvyys)
+def lisaa_listaan():
+    while True:
+        valinta = input("lisätäänkö sääasema(1) vai havainto(2) vai lopetetaanko(3)?")
+        if valinta == "1":
+            while True:
+                asema = SaaAsema.tee_saaasema()
+                saaasemat.append(asema)
+                uusi = input("Uusi havainto? (K/E): ")
+                if uusi.upper == "E" or uusi == "e":
+                    break
 
-print(fasfas.mphkm())
-print(fasfas.solmutkm())'''
+        elif valinta == "2":
+            while True:
+                havainto = SaaHavainto.tee_saahavainto()
+                saahavainnot.append(havainto)
+                uusi = input("Uusi havainto? (K/E): ")
+                if uusi.upper == "E" or uusi == "e":
+                    break
+        
+        elif valinta == "3":
+            break
+
+def tulosta():
+    for havainto in saahavainnot:
+        print("Havainnon päivämäärä:", havainto.paivamaara)
+        print("Havainnon lämpötila: ", havainto.lampotila)
+        print("Havainnon tuulennopeus: ", havainto.tnopeus)
+        print("Havainnon tuulensuunta: ", havainto.tsuunta)
+        print("Havainnon pilvisyys: ", havainto.pilvisyys)
+        print("Havainnon näkyvyys: ", havainto.nakyvyys)
+        print("Havainnon numero: ", havainto.numero, "\n")
+
+    for asema in saaasemat:
+        print("Aseman nimi: ", asema.nimi)
+        print("Aseman tyyppi: ", asema.tyyppi)
+        print("Aseman sijainti: ", asema.sijainti)
+        print("Aseman numero: ", asema.numero)
+
+saaasemat = []
+saahavainnot = []
+lisaa_listaan()
+tulosta()
